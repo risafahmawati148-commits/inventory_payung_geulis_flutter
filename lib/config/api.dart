2186e8f9a3f47953db2087b135e7a13245c64b55
Fauzi-vt <email-rifafauzi044@gmail.com>
2026-06-19 22:34:02 +0700

    Set API base URL to localhost

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,11 +1,5 @@
 class Api {
-  // === CONFIGURASI IP LARAVEL ===
-  // 1. Jika running di Web (Chrome) / Windows: "http://localhost:8000"
-  // 2. Jika running di Android Emulator (AVD): "http://10.0.2.2:8000"
-  // 3. Jika running di HP Fisik (satu jaringan Wi-Fi): "http://192.168.100.102:8000"
-  //    (PENTING: Jika menggunakan HP Fisik, jalankan Laravel dengan: php artisan serve --host=0.0.0.0)
-  
-  static const String baseUrl = "http://192.168.100.102:8000"; // Silakan sesuaikan dengan target running Anda
+  static const String baseUrl = "http://localhost:8000";
 
   /*
   |--------------------------------------------------------------------------
